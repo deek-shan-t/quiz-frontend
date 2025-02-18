5f2b08ad23554d754664990fea86cd30871f9dfc
deek-shan-t <singhdekson@gmail.com>
2025-02-18 16:20:39 +0530

    fix8

--- app/past_quizzes/page.tsx
+++ app/past_quizzes/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PastQuizzes from "./PastQuizzes";
-import Link from "next/link";
+// import Link from "next/link";
 
 const QuizzesPage = () => {
   return (
--- app/upcoming_quizzes/page.tsx
+++ app/upcoming_quizzes/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import UpcomingQuizzes from "./UpcomingQuizzes";
-import Link from "next/link";
+// import Link from "next/link";
 
 const QuizzesPage = () => {
   return (
